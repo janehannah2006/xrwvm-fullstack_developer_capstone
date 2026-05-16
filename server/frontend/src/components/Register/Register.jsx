2501136af5799fecf53e84427d570b67f1107d1d
janehannah2006 <727824tuam018@skct.edu.in>
2026-05-16 16:18:09 +0530

    Update Register.jsx

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -1,5 +1,26 @@
 function Register() {
-  return <h1>Register Page</h1>;
+  return (
+    <div>
+      <h1>Sign Up</h1>
+
+      <input type="text" placeholder="Username" />
+      <br /><br />
+
+      <input type="text" placeholder="First Name" />
+      <br /><br />
+
+      <input type="text" placeholder="Last Name" />
+      <br /><br />
+
+      <input type="email" placeholder="Email" />
+      <br /><br />
+
+      <input type="password" placeholder="Password" />
+      <br /><br />
+
+      <button>Register</button>
+    </div>
+  );
 }
 
 export default Register;
